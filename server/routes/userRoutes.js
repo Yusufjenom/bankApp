68409c0850e1ea2288b8a187efd8d360fe4ee27e
Yusufjenom <jenomy32@gmail.com>
2024-11-28 08:57:52 +0100

    update

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,59 +1,60 @@
-const userRouter = require('express').Router();
-const {signupUser,
-     loginUser,
-     loginForm,
-     signupForm,
-      homePage,
-       logoutUser,
-       displayUpdatePasswordEmail,
-       submitEmailForPasswordUpdate,
-       getUpdatePassword,
-       postUpdatedPassword,
-       creditCustomer,
-       generatePin,
-       resetPin,
-       getTransferForm,
-       comfirmUserToCredit,
-       getCurrentUserFromClientSide,
-       getUserByAccpountNum
-     } = require('../controller/userController');
-const {verifyUser} = require('../middleware/authUser');
+const userRouter = require("express").Router();
+const {
+  signupUser,
+  loginUser,
+  loginForm,
+  signupForm,
+  homePage,
+  logoutUser,
+  displayUpdatePasswordEmail,
+  submitEmailForPasswordUpdate,
+  getUpdatePassword,
+  postUpdatedPassword,
+  creditCustomer,
+  generatePin,
+  resetPin,
+  getTransferForm,
+  comfirmUserToCredit,
+  getCurrentUserFromClientSide,
+  getUserByAccpountNum,
+} = require("../controller/userController");
+const { verifyUser } = require("../middleware/authUser");
 
 //SIGN UP A USER
-userRouter.post('/signup-user', signupUser);
+userRouter.post("/signup-user", signupUser);
 
-userRouter.post('/login-user', loginUser);
+userRouter.post("/login-user", loginUser);
 
-userRouter.get('/login-user', loginForm);
+userRouter.get("/login-user", loginForm);
 
-userRouter.get('/signup-user', signupForm);
+userRouter.get("/signup-user", signupForm);
 
-userRouter.get('/submit-email', displayUpdatePasswordEmail);
+userRouter.get("/submit-email", displayUpdatePasswordEmail);
 
-userRouter.post('/submitted', submitEmailForPasswordUpdate);
+userRouter.post("/submitted", submitEmailForPasswordUpdate);
 
-userRouter.get('/update-password/:id/:token', getUpdatePassword);
+userRouter.get("/update-password/:id/:token", getUpdatePassword);
 
-userRouter.put('/update-password/:id/:token', postUpdatedPassword);
+userRouter.put("/update-password/:id/:token", postUpdatedPassword);
 
-userRouter.get('/current-user', getCurrentUserFromClientSide);
+userRouter.get("/current-user", getCurrentUserFromClientSide);
 
-userRouter.post('/get-user-by-account', getUserByAccpountNum);
+userRouter.post("/get-user-by-account", getUserByAccpountNum);
 
 //userRouter.use(verifyUser);
 
-userRouter.get('/home',  homePage);
+userRouter.get("/home", homePage);
 
-userRouter.get('/logout-user', logoutUser);
+userRouter.get("/logout-user", logoutUser);
 
-userRouter.put('/credit-customer',  creditCustomer);
+userRouter.put("/credit-customer", creditCustomer);
 
-userRouter.put('/set-pin', generatePin);
+userRouter.put("/set-pin", generatePin);
 
-userRouter.put('/reset-pin', resetPin);
+userRouter.put("/reset-pin", resetPin);
 
-userRouter.get('/transfer', getTransferForm);
+userRouter.get("/transfer", getTransferForm);
 
-userRouter.post('/comfirm', comfirmUserToCredit)
+userRouter.post("/comfirm", comfirmUserToCredit);
 
 module.exports = userRouter;
