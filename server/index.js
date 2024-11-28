d6d1e80a8dfbd5908d401a7994dc6e3b75a0029c
Yusufjenom <jenomy32@gmail.com>
2024-11-28 08:57:08 +0100

    update

--- server/index.js
+++ server/index.js
@@ -1,55 +1,59 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-require('dotenv').config();
-const { connectDB } = require('./database/db');
-const userRouter = require('./routes/userRoutes');
-const adminRouter = require('./routes/adminRoutes');
-const {ErrorHandler} = require('./middleware/ErrorHandler');
-const methodOverride = require('method-override');
-const session = require('express-session');
-const memoryStore = require('memorystore')(session);
-const cors = require('cors');
-const helmet = require('helmet');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+const { connectDB } = require("./database/db");
+const userRouter = require("./routes/userRoutes");
+const adminRouter = require("./routes/adminRoutes");
+const { ErrorHandler } = require("./middleware/ErrorHandler");
+const methodOverride = require("method-override");
+const session = require("express-session");
+const memoryStore = require("memorystore")(session);
+const cors = require("cors");
+const helmet = require("helmet");
 
 const app = express();
 
 const port = process.env.PORT || 8080;
 
 //middleware
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 //app.use(helmet.crossOriginResourcePolicy({policy:"cross_origin"}))
-app.use(cors({
-    origin: "http://localhost:5176",
-    credentials: true
-}))
+app.use(
+  cors({
+    origin: "http://localhost:5175",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 //app.set('view engine', 'ejs')
 
-app.use(express.static('public'));
-app.use(methodOverride('_method'));
-app.use('/api/v1', userRouter);
-app.use('/api/v2', adminRouter);
+app.use(express.static("public"));
+app.use(methodOverride("_method"));
+app.use("/api/v1", userRouter);
+app.use("/api/v2", adminRouter);
 
-
-app.use(session({
+app.use(
+  session({
     secret: "secret",
     resave: false,
     saveUninitialized: true,
     store: new memoryStore({
-        checkPeriod: 86400000
-    })
-}));
+      checkPeriod: 86400000,
+    }),
+  })
+);
 
 //Error handler middleware has to be the last middleware on your stack
 app.use(ErrorHandler);
 
 (async function () {
-    try {
-        await connectDB()
-        app.listen(port, () => console.log(`server running on localhost port ${port}`));
-    }
-    catch (err) {
-        console.log(err.message)
-    }
-})()
+  try {
+    await connectDB();
+    app.listen(port, () =>
+      console.log(`server running on localhost port ${port}`)
+    );
+  } catch (err) {
+    console.log(err.message);
+  }
+})();
