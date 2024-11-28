2ef993bcaa4e3ead6300908e6e6c11f2d1d2d826
Yusufjenom <jenomy32@gmail.com>
2024-11-28 08:56:07 +0100

    update

--- client/src/modules/auth/pages/login/index.jsx
+++ client/src/modules/auth/pages/login/index.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useState } from 'react';
 import useLoginUser from '../../hooks/useLoginUser';
 import { loginReducer } from '../../utils';
 import {useNavigate} from 'react-router-dom'
@@ -6,6 +6,8 @@ import {useNavigate} from 'react-router-dom'
 
 function LoginUser() {
     const [state, dispatch] = useReducer(loginReducer, { email: "", password: "" });
+    const [emailError, setEmailError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
     const { loginResponse, handleLogin } = useLoginUser();
     const navigate = useNavigate()
 
@@ -16,20 +18,26 @@ function LoginUser() {
         console.log(loginResponse)
         if(loginResponse.data.success){
             localStorage.setItem('userToken', JSON.stringify(loginResponse.data.token));
-            console.log("hello world")
             navigate('/dashboard')
+        }else{
+            setEmailError(loginResponse?.data.error.email);
+            setPasswordError(loginResponse?.data.error.password);
         }
         
         
     };
-    
+    console.log(emailError);
+    console.log(passwordError);
     return (
         <div>
             <h1>Login Page</h1>
-            <form onSubmit={handleSubmit}>
-                <input type="email" onChange={(e) => dispatch({ type: 'EMAIL', payload: e.target.value })} />
-                <input type="password" onChange={(e) => dispatch({ type: 'PASSWORD', payload: e.target.value })} />
-                <input type="submit" value="Login" />
+            <form onSubmit={handleSubmit} style={{display:"flex", flexDirection:"column", gap:".5rem", width:"100vw", justifyContent:"center", alignItems:"center"}}>
+                <input type="email" onChange={(e) => dispatch({ type: 'EMAIL', payload: e.target.value })} style={{width:"35%"}} />
+                <input type="password" onChange={(e) => dispatch({ type: 'PASSWORD', payload: e.target.value })} style={{width:"35%"}}/>
+                <p style={{color:"red"}}>{passwordError}</p>
+                <p style={{color:"red"}}>{emailError}</p>
+                {/* <input type="submit" value="Login" style={{width:"35%"}}/> */}
+                <button style={{width:"35%"}}>Login</button>
             </form>
         </div>
     )
